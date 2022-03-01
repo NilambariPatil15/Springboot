039dfc68301f06bfb74d548ca1f28e139d969306
NilambariPatil15 <patilnilambarid15498@gmail.com>
2022-03-01 16:55:13 +0530

    initial commit

---
+++ src/main/java/com/demo1/spring_boot_demo/SpringJenkinsProjectApplication.java
@@ -0,0 +1,13 @@
+package com.demo1.spring_boot_demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringJenkinsProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringJenkinsProjectApplication.class, args);
+	}
+
+}
